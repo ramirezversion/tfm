bba6b01e7dd728bf0e383613b1ac8d7d016aa036
Unknown <ramirezversion@gmail.com>
2018-03-15 23:37:09 +0100

    Auto stash before merge of "master" and "origin/master"

--- tfm/resources/views/layouts/app.blade.php
+++ tfm/resources/views/layouts/app.blade.php
@@ -7,17 +7,22 @@
     </head>
     <body>
         @include('inc.navbar')
-        
+
         <div class="container">
-            @include('inc.showcase')
+            @if(Request::is('/'))
+              @include('inc.showcase')
+            @endif
             <div class="row">
                 <div class="col-md-8 col-lg-8">
-                    @yield('content') 
+                    @yield('content')
                 </div>
                 <div class="col-md-4 col-lg-4">
                     @include('inc.sidebar')
-                </div>       
+                </div>
             </div>
         </div>
+        <footer id="footer" class="text-center">
+          <p>Copyright 2018 &copy; Acme</p>
+        </footer>
     </body>
 </html>
