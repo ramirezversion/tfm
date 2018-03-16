745dfbabc46aba642579c55c8bec48a5cd944209
Unknown <ramirezversion@gmail.com>
2018-03-16 22:24:53 +0100

    zz
    zz

--- tfm/resources/views/layouts/loginapp.blade.php
+++ tfm/resources/views/layouts/loginapp.blade.php
@@ -2,39 +2,19 @@
 <html>
     <head>
         <meta charset="utf-8">
-        <title>Acme Login</title>
+        <title>Acme Log in</title>
         <link rel="stylesheet" href="/css/app.css">
     </head>
     <body>
+        @include('inc.navbarlogin')
 
-      <div class="text-center" "container">
-
-        {!! Form::open(['url' => 'login']) !!}
-
-          <div class="form-signin">
-             <img class="mb-4" src="https://getbootstrap.com/assets/brand/bootstrap-solid.svg" alt="" width="72" height="72">
-             <h1 class="h3 mb-3 font-weight-normal">Please log in</h1>
-
-             <div class="form-group">
-               {{Form::label('username', 'Username', ['for' => 'inputEmail', 'class' => 'sr-only'])}}
-               {{Form::text('username', '', ['class' => 'form-control', 'placeholder' => 'Username', 'required', 'autofocus'])}}
-             </div>
-
-             <div class="form-group">
-               {{Form::label('password', 'Password', ['for' => 'inputPassword', 'class' => 'sr-only'])}}
-               {{Form::text('password', '', ['class' => 'form-control', 'placeholder' => 'Password', 'required'])}}
-             </div>
-
-             <div>
-               {{Form::submit('Log in', ['class' => 'btn btn-lg btn-primary btn-block'])}}
-             </div>
-
-           </div>
-
-         {!! Form::close() !!}
-
-      </div>
-
+        <div class="container" "text-center">
+            <div class="row">
+                <div class="col-md-8 col-lg-8">
+                  @yield('content')
+                </div>
+            </div>
+        </div>
         <footer id="footer" class="text-center">
           <p>Copyright 2018 &copy; Acme</p>
         </footer>
--- tfm/resources/views/login.blade.php
+++ tfm/resources/views/login.blade.php
@@ -1,3 +1,27 @@
+@extends('layouts.loginapp')
+
 @section('content')
-  @extends('layouts.loginapp')
+    <h1>Log In</h1>
+
+    <img class="mb-4" src="https://getbootstrap.com/assets/brand/bootstrap-solid.svg" alt="" width="72" height="72">
+    <h1 class="h3 mb-3 font-weight-normal">Please log in</h1>
+
+    {!! Form::open(['url' => 'login']) !!}
+
+      <div class="form-group">
+        {{Form::label('username', 'Username', ['for' => 'inputEmail', 'class' => 'sr-only'])}}
+        {{Form::text('username', '', ['class' => 'form-control', 'placeholder' => 'Username', 'required', 'autofocus'])}}
+      </div>
+
+      <div class="form-group">
+        {{Form::label('password', 'Password', ['for' => 'inputPassword', 'class' => 'sr-only'])}}
+        {{Form::text('password', '', ['class' => 'form-control', 'placeholder' => 'Password', 'required'])}}
+      </div>
+
+      <div>
+        {{Form::submit('Log in', ['class' => 'btn btn-primary'])}}
+      </div>
+
+
+    {!! Form::close() !!}
 @endsection
