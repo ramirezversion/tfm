144c5b2c7a134b671d03e45ec63486884b50f73c
Antonio Ramirez <ramirezversion@gmail.com>
2018-03-17 17:51:51 +0100

    ho

--- tfm/resources/views/auth/loginauth.blade.php
+++ tfm/resources/views/auth/loginauth.blade.php
@@ -5,7 +5,7 @@
     <img class="mb-4" src="https://getbootstrap.com/assets/brand/bootstrap-solid.svg" alt="" width="72" height="72">
     <h1 class="h3 mb-3 font-weight-normal">Please Log In</h1>
 
-    {!! Form::open(['route' => 'handlelogin']) !!}
+    {!! Form::open(['url' => 'login']) !!}
 
       <div class="form-group">
         {{Form::label('username', 'Username', ['class' => 'sr-only'])}}
