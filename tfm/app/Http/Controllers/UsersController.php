f7dd89c7b4f534e8185c3caa87f65eb2d99b7ec7
Antonio Ramirez <ramirezversion@gmail.com>
2018-03-17 22:49:37 +0100

    zz

--- tfm/app/Http/Controllers/UsersController.php
+++ tfm/app/Http/Controllers/UsersController.php
@@ -4,6 +4,9 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use Illuminate\Support\Facades\Auth;
+use App\User;
+
 class UsersController extends Controller
 {
     /**
@@ -82,7 +85,7 @@ class UsersController extends Controller
         //
     }
 
-    public function home(){
+    public function getHome(){
 
       if(Auth::check()){
         return view('users.home');
