0dc3289aa2d2d6c85e53cc6fdce05a08da2e9736
Antonio Ramirez <ramirezversion@gmail.com>
2018-03-18 19:32:23 +0100

    zz

--- tfm/resources/views/users/login_registers.blade.php
+++ tfm/resources/views/users/login_registers.blade.php
@@ -2,10 +2,13 @@
 
 @section('content')
     <h1>Logins log</h1>
+
+    <p>This is the register of the logins actions.</p>
+
     @if(count($loginEntries) > 0)
 
       <table class="table table-sm table-hover table-striped">
-        <thead class="thead-light">
+        <thead class="thead-dark">
           <tr>
             <th scope="col">Username</th>
             <th scope="col">Date</th>
@@ -25,14 +28,5 @@
         </tbody>
       </table>
 
-      <!-- @foreach($loginEntries as $loginregister)
-        <ul class="list-group">
-          <li class="list-group-item">Username: {{$loginregister->username}}</li>
-          <li class="list-group-item">Date: {{$loginregister->date}}</li>
-          <li class="list-group-item">Time: {{$loginregister->time}}</li>
-          <li class="list-group-item">Action: {{$loginregister->action}}</li>
-        </ul>
-      @endforeach -->
-      
     @endif
 @endsection
