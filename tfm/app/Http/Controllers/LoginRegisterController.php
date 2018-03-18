f403cfad1ecadc6c641ad4fe40384704a508e17a
Antonio Ramirez <ramirezversion@gmail.com>
2018-03-18 01:01:33 +0100

    zz

--- tfm/app/Http/Controllers/LoginRegisterController.php
+++ tfm/app/Http/Controllers/LoginRegisterController.php
@@ -8,22 +8,6 @@ use App\LoginRegister;
 class LoginRegisterController extends Controller
 {
 
-  public function submitLoginRegister(){
-
-    // Create new entry to save the login and the date/time
-    $loginEntry = new LoginRegister;
-    $now = new DateTime();
-
-    $loginEntry->username = {{Auth::user()->username}};
-    $loginEntry->date = $now->format('Y-m-d');
-    $loginEntry->time = $now->format('H:i:s');
-
-    // Save message
-    $loginEntry->save();
-    return;
-
-  }
-
   public function getLoginRegisters(){
 
     $loginEntries = LoginRegister::all();
--- tfm/app/LoginRegister.php
+++ tfm/app/LoginRegister.php
@@ -4,7 +4,28 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
+use Illuminate\Http\Request;
+
+use Illuminate\Support\Facades\Auth;
+use App\User;
+
 class LoginRegister extends Model
 {
-    //
+
+  public function submitLoginRegister(){
+
+    // Create new entry to save the login and the date/time
+    $loginEntry = new LoginRegister;
+    $now = new DateTime();
+
+    $loginEntry->username = {{Auth::user()->username}};
+    $loginEntry->date = $now->format('Y-m-d');
+    $loginEntry->time = $now->format('H:i:s');
+
+    // Save message
+    $loginEntry->save();
+    return;
+
+  }
+
 }
