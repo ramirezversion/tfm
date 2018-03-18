f0356a854f6e1c9e6454f57db9a5d6049e65a1ce
Antonio Ramirez <ramirezversion@gmail.com>
2018-03-18 19:30:06 +0100

    zz

--- tfm/resources/views/users/login_registers.blade.php
+++ tfm/resources/views/users/login_registers.blade.php
@@ -3,13 +3,36 @@
 @section('content')
     <h1>Logins log</h1>
     @if(count($loginEntries) > 0)
-      @foreach($loginEntries as $loginregister)
+
+      <table class="table table-sm table-hover table-striped">
+        <thead class="thead-light">
+          <tr>
+            <th scope="col">Username</th>
+            <th scope="col">Date</th>
+            <th scope="col">Time</th>
+            <th scope="col">Action</th>
+          </tr>
+        </thead>
+        <tbody>
+          @foreach($loginEntries as $loginregister)
+            <tr>
+                <th scope="row">{{$loginregister->username}}</th>
+                <td>{{$loginregister->date}}</td>
+                <td>{{$loginregister->time}}</td>
+                <td>{{$loginregister->action}}</td>
+            </tr>
+          @endforeach
+        </tbody>
+      </table>
+
+      <!-- @foreach($loginEntries as $loginregister)
         <ul class="list-group">
           <li class="list-group-item">Username: {{$loginregister->username}}</li>
           <li class="list-group-item">Date: {{$loginregister->date}}</li>
           <li class="list-group-item">Time: {{$loginregister->time}}</li>
           <li class="list-group-item">Action: {{$loginregister->action}}</li>
         </ul>
-      @endforeach
+      @endforeach -->
+      
     @endif
 @endsection
