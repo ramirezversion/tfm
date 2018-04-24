4229b2fd056ac287584302becf04159c6e3379be
ramirezversion <ramirezversion@gmail.com>
2018-04-24 12:47:40 +0200

    angular_to_an_include_file

--- tfm/resources/views/users/dashboard.blade.php
+++ tfm/resources/views/users/dashboard.blade.php
@@ -21,7 +21,15 @@
 
     <h3>Realtime status</h3>
 
-    @include('inc.dashboardangular')
+    <p>
+      <div ng-app="GaugeDashApp" ng-controller="GaugeDashController">
+          <div id="gaugeMemory" dx-circular-gauge="gauge.memoryDash"></div>
+          <div id="gaugeDisk" dx-circular-gauge="gauge.diskDash"></div>
+          <div id="gaugeCPU" dx-circular-gauge="gauge.cpuDash"></div>
+          <div id="gaugeNumProceses" dx-linear-gauge="gauge.numProcesesDash"></div>
+          <div id="gaugeNumCores" dx-linear-gauge="gauge.numCoresDash"></div>
+      </div>
+    </p>
 
     <div>
       @if(count($dashboard) > 0)
